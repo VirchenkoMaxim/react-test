1f9301ffe49bacb47dc5d70c827aa6920edcebcc
Maxym <maks064040@gmail.com>
2020-07-19 08:24:51 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
