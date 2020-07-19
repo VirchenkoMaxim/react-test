24afb3a9c7b7ac44d312147f323af9150e7639b8
Maxym <maks064040@gmail.com>
2020-07-19 13:49:59 +0300

    h10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Stats from './components/Stats';
 
 function App() {
   return (
     <div className="App">
       <div>hi</div>
-      {/* <Stats /> */}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://app-coo-back-graphql.herokuapp.com/graphql',
-  cache: new InMemoryCache()
-});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <App />,
   document.getElementById('root')
 );
 
