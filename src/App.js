8044bf9c4ef0446f4352271057b49e79203963c1
Maxym <maks064040@gmail.com>
2020-07-19 13:35:37 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import Stats from './components/Stats';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Stats />
-      <Footer />
+      <div>hi</div>
+      {/* <Stats /> */}
     </div>
   );
 }
